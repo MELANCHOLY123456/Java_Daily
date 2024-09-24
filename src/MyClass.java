b25a071d361636aaf1731ecf9f6a9cfe1dfaed47
MELANCHOLY123456 <moulengdangubulei@gmail.com>
2024-09-24 21:02:39 +0800

    MyClass

---
+++ src/MyClass.java
@@ -0,0 +1,19 @@
+class MyClass {
+    int x;
+
+    MyClass() {
+        x = 10;
+    }
+
+    MyClass(int i) {
+        x = i;
+    }
+}
+
+class MyClass_Demo {
+    public static void main(String[] args) {
+        MyClass obj1 = new MyClass();
+        MyClass obj2 = new MyClass(15);
+        System.out.println(obj1.x + " " + obj2.x);
+    }
+}
