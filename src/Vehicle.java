9a29e7bc96cb40f778ba606f32fb113cebe54957
MELANCHOLY123456 <moulengdangubulei@gmail.com>
2024-09-24 21:08:09 +0800

    Vehicle_第四次更改

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -3,6 +3,12 @@ class Vehicle {
     int fuel_cap;
     int mpg;
 
+    Vehicle(int p, int f, int m) {
+        passengers = p;
+        fuel_cap = f;
+        mpg = m;
+    }
+
     int range() {
         return fuel_cap * mpg;
     }
@@ -14,26 +20,18 @@ class Vehicle {
 
 class VehicleDemo {
     public static void main(String[] args) {
-        Vehicle minivan = new Vehicle();
-        Vehicle sport_scar = new Vehicle();
+        Vehicle minivan = new Vehicle(7, 16, 21);
+        Vehicle sport_scar = new Vehicle(2, 14, 12);
         double gallons;
         int dist = 252;
 
-        int range_01, range_02;
-
-        minivan.passengers = 7;
-        minivan.fuel_cap = 16;
-        minivan.mpg = 21;
-
-        sport_scar.passengers = 2;
-        sport_scar.fuel_cap = 14;
-        sport_scar.mpg = 12;
-
-        range_01 = minivan.range();
-        range_02 = sport_scar.range();
-
-        System.out.println("Minivan can carry " + minivan.passengers + " with range of " + range_01 + " Miles");
-        System.out.println("Sport_scar can carry " + sport_scar.passengers + " with range of " + range_02 + " miles");
+//        int range_01, range_02;
+//
+//        range_01 = minivan.range();
+//        range_02 = sport_scar.range();
+//
+//        System.out.println("Minivan can carry " + minivan.passengers + " with range of " + range_01 + " Miles");
+//        System.out.println("Sport_scar can carry " + sport_scar.passengers + " with range of " + range_02 + " miles");
 
         gallons = minivan.fuel_needed(dist);
         System.out.println("To go " + dist + " miles minivan needs " + gallons + " gallons of fuel.");
