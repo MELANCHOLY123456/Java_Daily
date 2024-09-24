dffb3f1a48ad63b7dcc01297808f486875cd2a5c
MELANCHOLY123456 <moulengdangubulei@gmail.com>
2024-09-24 20:36:43 +0800

    Vehicle_第三次更改

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -6,12 +6,18 @@ class Vehicle {
     int range() {
         return fuel_cap * mpg;
     }
+
+    double fuel_needed(int miles) {
+        return (double) miles / mpg;
+    }
 }
 
 class VehicleDemo {
     public static void main(String[] args) {
         Vehicle minivan = new Vehicle();
         Vehicle sport_scar = new Vehicle();
+        double gallons;
+        int dist = 252;
 
         int range_01, range_02;
 
@@ -28,5 +34,11 @@ class VehicleDemo {
 
         System.out.println("Minivan can carry " + minivan.passengers + " with range of " + range_01 + " Miles");
         System.out.println("Sport_scar can carry " + sport_scar.passengers + " with range of " + range_02 + " miles");
+
+        gallons = minivan.fuel_needed(dist);
+        System.out.println("To go " + dist + " miles minivan needs " + gallons + " gallons of fuel.");
+
+        gallons = sport_scar.fuel_needed(dist);
+        System.out.println("To go " + dist + " miles sport_scar needs " + gallons + " gallons of fuel.");
     }
 }
