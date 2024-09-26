9ecd547aa717db5f4ee75bbeb40ea8483de51d50
MELANCHOLY123456 <moulengdangubulei@gmail.com>
2024-09-26 19:40:16 +0800

    TwoDShape_第六次修改

--- src/Chapter7/TwoDShape.java
+++ src/Chapter7/TwoDShape.java
@@ -67,6 +67,23 @@ class Triangle extends TwoDShape {
     }
 }
 
+class ColorTriangle extends Triangle {
+    private final String color;
+
+    ColorTriangle(String c, String s, double w, double h) {
+        super(s, w, h);
+        color = c;
+    }
+
+    String getColor() {
+        return color;
+    }
+
+    void showColor() {
+        System.out.println("Color is " + color);
+    }
+}
+
 class Rectangle extends TwoDShape {
     Rectangle(double w, double h) {
         // super()必须是在子类构造函数中执行的第一条语句
@@ -119,5 +136,28 @@ class Shapes {
         r2.showDim();
         System.out.println("Is square? " + r2.isSquare());
         System.out.println("Area is " + r2.area());
+    }
+}
+
+class ShapesDemo {
+    public static void main(String[] args) {
+        ColorTriangle t1 =
+                new ColorTriangle("Blue", "outlined", 8.0, 12.0);
+        ColorTriangle t2 =
+                new ColorTriangle("Red", "filled", 4.0, 2.0);
+
+        System.out.println("Info for t1: ");
+        t1.showStyle();
+        t1.showDim();
+        t1.showColor();
+        System.out.println("Area is " + t1.area());
+
+        System.out.println();
+
+        System.out.println("Info for t2: ");
+        t2.showStyle();
+        t2.showDim();
+        t2.showColor();
+        System.out.println("Area is " + t2.area());
     }
 }
