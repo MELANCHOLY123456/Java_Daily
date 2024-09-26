8ff4fb8c62a4409f6c3f194de0538b2df2089eeb
MELANCHOLY123456 <moulengdangubulei@gmail.com>
2024-09-26 20:32:54 +0800

    A3_第二次修改

--- src/Chapter7/A3.java
+++ src/Chapter7/A3.java
@@ -22,6 +22,7 @@ class B3 extends A3 {
     }
 
     void show() {
+        super.show(); // this calls A's show()
         System.out.println("k: " + k);
     }
 }
@@ -33,3 +34,34 @@ class Override {
         subOb.show(); // this calls show() in B
     }
 }
+
+/* Methods with differing type signatures are
+   overloaded and not overridden. */
+//class A {
+//    int i, j;
+//
+//    A(int a, int b) {
+//        i = a;
+//        j = b;
+//    }
+//
+//    // display i and j
+//    void show() {
+//        System.out.println("i and j: " + i + " " + j);
+//    }
+//}
+//
+//// Create a subclass by extending class A.
+//class B extends A {
+//    int k;
+//
+//    B(int a, int b, int c) {
+//        super(a, b);
+//        k = c;
+//    }
+//
+//    // overload show()
+//    void show(String msg) {
+//        System.out.println(msg + k);
+//    }
+//}
