c7c705893e662150891e5f4d951b95654165fabb
MELANCHOLY123456 <moulengdangubulei@gmail.com>
2024-09-26 20:33:40 +0800

    A3_第二次修改

--- src/Chapter7/A3.java
+++ src/Chapter7/A3.java
@@ -65,3 +65,12 @@ class Override {
 //        System.out.println(msg + k);
 //    }
 //}
+
+//class Overload {
+//    public static void main(String args[]) {
+//        B subOb = new B(1, 2, 3);
+//
+//        subOb.show("This is k: "); // this calls show() in B
+//        subOb.show(); // this calls show() in A
+//    }
+//}
