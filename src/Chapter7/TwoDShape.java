c598d1484b600a2813a712cb98ea95f3b01bf3bf
MELANCHOLY123456 <moulengdangubulei@gmail.com>
2024-09-26 21:05:31 +0800

    TwoDShape_第九次修改

--- src/Chapter7/TwoDShape.java
+++ src/Chapter7/TwoDShape.java
@@ -1,6 +1,6 @@
 package Chapter7;
 
-class TwoDShape {
+abstract class TwoDShape {
     private double width;
     private double height;
     private String name;
@@ -52,10 +52,7 @@ class TwoDShape {
         System.out.println("Width and height are " + width + " and " + height);
     }
 
-    double area() {
-        System.out.println("area() must be overridden");
-        return 0.0;
-    }
+    abstract double area();
 }
 
 class Triangle extends TwoDShape {
@@ -227,7 +224,25 @@ class DynShapes {
         shapes[1] = new Rectangle(10);
         shapes[2] = new Rectangle(10, 4);
         shapes[3] = new Triangle(7.0);
-        shapes[4] = new TwoDShape(10, 20, "generic");
+//        shapes[4] = new TwoDShape(10, 20, "generic");
+
+        for (TwoDShape shape : shapes) {
+            System.out.println("object is " + shape.getName());
+            System.out.println("Area is " + shape.area());
+
+            System.out.println();
+        }
+    }
+}
+
+class AbsShape {
+    public static void main(String[] args) {
+        TwoDShape[ ] shapes = new TwoDShape[4];
+
+        shapes[0] = new Triangle("outlined", 8.0, 12.0);
+        shapes[1] = new Rectangle(10);
+        shapes[2] = new Rectangle(10, 4);
+        shapes[3] = new Triangle(7.0);
 
         for (TwoDShape shape : shapes) {
             System.out.println("object is " + shape.getName());
