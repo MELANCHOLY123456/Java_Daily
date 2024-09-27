7c36c446b72cf90bca10a9404a5170cdf3dad647
MELANCHOLY123456 <moulengdangubulei@gmail.com>
2024-09-27 15:20:13 +0800

    ExcTest_第一次修改

---
+++ src/Chapter9/ExcTest.java
@@ -0,0 +1,33 @@
+package Chapter9;
+
+public class ExcTest {
+    static void genException() {
+        int[] num = new int[4];
+        System.out.println("Before exception is generated.");
+
+        for (int i = 0; i < num.length; i++) {
+            num[i] = i + 1;
+            System.out.print(num[i] + " ");
+        }
+        System.out.println();
+
+        // generate an index out-of-bounds exception
+        num[7] = 10;
+        System.out.println("this won't be displayed");
+    }
+}
+
+class ExcDemo2 {
+    public static void main(String[] args) {
+        try {
+            ExcTest.genException();
+        } catch (ArrayIndexOutOfBoundsException exc) {
+            // catch the exception
+            System.out.println("Index out-of-bounds!");
+        }
+        System.out.println("After catch statement.");
+    }
+}
+
+// 从try代码块调用的方法抛出的异常可以被与try代码块匹配的catch语句捕获
+// 当然这是假定方法自己没有捕获异常
