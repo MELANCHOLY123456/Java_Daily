bf0ec897c9af29d6285bbc067b2c27e0bb712fd9
MELANCHOLY123456 <moulengdangubulei@gmail.com>
2024-09-28 09:27:47 +0800

    WriteDemo_第二次修改

--- src/Chapter10/WriteDemo.java
+++ src/Chapter10/WriteDemo.java
@@ -3,10 +3,13 @@ package Chapter10;
 public class WriteDemo {
     public static void main(String[] args) {
         int b = 'X';
-        System.out.write(b);
-        System.out.write('\n');
+        System.out.println(b);
+        System.out.write(88);
+        System.out.flush(); // 刷新输出流 -> 确保输出立即显示
     }
 }
 
-// System.out.write() 方法需要一个整数作为参数，它会将该整数解释为一个字节值并输出对应的字符
-// System.out.println() 可以更简便地输出带换行的字符串
+/*
+ * System.out.write()方法需要一个整数作为参数，它会将该整数解释为一个字节值并输出对应的字符
+ * System.out.println()可以更简便地输出带换行的字符串
+ * */
