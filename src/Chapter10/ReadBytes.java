759de2556e2b8e0342960cc1f2804a1dbd027256
MELANCHOLY123456 <moulengdangubulei@gmail.com>
2024-09-28 09:18:50 +0800

    ReadBytes_第三次修改

--- src/Chapter10/ReadBytes.java
+++ src/Chapter10/ReadBytes.java
@@ -2,6 +2,11 @@ package Chapter10;
 
 import java.io.IOException;
 
+/*
+ * 可能发生的任何I/O异常都被抛出到main()以外 -> 这种方法在从控制台读取数据时
+ * 十分常见，但是如果愿意也可以自己处理这些类型的错误
+ * */
+
 public class ReadBytes {
     public static void main(String[] args) throws IOException {
         byte[] data = new byte[10];
@@ -21,6 +26,6 @@ public class ReadBytes {
 }
 
 /*
-* 在Java中，System.in.read(byte[] b) 方法从标准输入流中
-* 读取一些字节数并将它们存储在数组 b 中。它返回实际读取的字节数。
-*  */
+ * 在Java中，System.in.read(byte[] b) 方法从标准输入流中
+ * 读取一些字节数并将它们存储在数组 b 中。它返回实际读取的字节数。
+ *  */
