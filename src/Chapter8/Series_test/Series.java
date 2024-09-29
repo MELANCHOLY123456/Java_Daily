0db96e96bf96dbee7113c9a561b396847fe0f472
MELANCHOLY123456 <moulengdangubulei@gmail.com>
2024-09-29 17:42:23 +0800

    Series_第二次修改

--- src/Chapter8/Series_test/Series.java
+++ src/Chapter8/Series_test/Series.java
@@ -3,6 +3,14 @@ package Chapter8.Series_test;
 public interface Series {
     int getNext();
 
+    default int[] getNextArray(int n) {
+        int[] vals = new int[n];
+        for (int i = 0; i < n; i++) {
+            vals[i] = getNext();
+        }
+        return vals;
+    }
+
     void reset();
 
     void setStart(int x);
