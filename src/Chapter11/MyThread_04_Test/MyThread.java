708d5c44485316ea25d8913fd77cc37f47c627d6
MELANCHOLY123456 <moulengdangubulei@gmail.com>
2024-09-29 20:46:19 +0800

    MyThread_04_第一次修改

---
+++ src/Chapter11/MyThread_04_Test/MyThread.java
@@ -0,0 +1,28 @@
+package Chapter11.MyThread_04_Test;
+
+public class MyThread extends Thread {
+    Thread thread;
+
+    MyThread(String name) {
+        thread = new Thread(this, name);
+    }
+
+    public static MyThread createAndStart(String name) {
+        MyThread myThread = new MyThread(name);
+        myThread.thread.start();
+        return myThread;
+    }
+
+    public void run() {
+        System.out.println(thread.getName() + " starting.");
+        try {
+            for (int count = 0; count < 10; count++) {
+                Thread.sleep(400);
+                System.out.println("In " + thread.getName() + ", count is " + count);
+            }
+        } catch (InterruptedException exc) {
+            System.out.println(thread.getName() + " interrupted.");
+        }
+        System.out.println(thread.getName() + " terminating.");
+    }
+}
