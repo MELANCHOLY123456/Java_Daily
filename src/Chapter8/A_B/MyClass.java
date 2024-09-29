916fe4863a7df0cb82ad664239abd3132e7852dd
MELANCHOLY123456 <moulengdangubulei@gmail.com>
2024-09-29 17:31:30 +0800

    A_B_第二次修改

--- src/Chapter8/A_B/MyClass.java
+++ src/Chapter8/A_B/MyClass.java
@@ -13,12 +13,3 @@ public class MyClass implements B {
         System.out.println("Implement meth3().");
     }
 }
-
-class IFExtend {
-    public static void main(String[] args) {
-        MyClass obj = new MyClass();
-        obj.meth1();
-        obj.meth2();
-        obj.meth3();
-    }
-}
