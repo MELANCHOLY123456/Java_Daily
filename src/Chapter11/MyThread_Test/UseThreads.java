a7889f188463066405e2ba76dbb5ce191c4d8526
MELANCHOLY123456 <moulengdangubulei@gmail.com>
2024-09-29 19:21:50 +0800

    MyThread_第一次修改

---
+++ src/Chapter11/MyThread_Test/UseThreads.java
@@ -0,0 +1,19 @@
+package Chapter11.MyThread_Test;
+
+public class UseThreads {
+    public static void main(String[] args) {
+        System.out.println("Main thread starting.");
+        MyThread mt = new MyThread("Child #1");
+        Thread newThread = new Thread(mt);
+        newThread.start();
+        for (int i = 0; i < 50; i++) {
+            System.out.print(".");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException exc) {
+                System.out.println("Main thread interrupted.");
+            }
+        }
+        System.out.println("Main thread ending.");
+    }
+}
