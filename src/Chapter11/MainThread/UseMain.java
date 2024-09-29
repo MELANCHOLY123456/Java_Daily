7dae10f80e5b60402ef9fdfc04692392cad58978
MELANCHOLY123456 <moulengdangubulei@gmail.com>
2024-09-29 22:39:12 +0800

    UseMain_第一次修改

---
+++ src/Chapter11/MainThread/UseMain.java
@@ -0,0 +1,20 @@
+package Chapter11.MainThread;
+
+public class UseMain {
+    public static void main(String[] args) {
+        Thread thread;
+        // Get the main thread
+        thread = Thread.currentThread();
+        // Display main thread's name
+        System.out.println("Main thread is called: " + thread.getName());
+        // Display main thread's priority.
+        System.out.println("Priority: " + thread.getPriority());
+        System.out.println();
+        // Set the name and priority.
+        System.out.println("Setting name and priority.\n");
+        thread.setName("Thread #1");
+        thread.setPriority(Thread.NORM_PRIORITY + 3);
+        System.out.println("Main thread is now called: " + thread.getName());
+        System.out.println("Priority is now: " + thread.getPriority());
+    }
+}
