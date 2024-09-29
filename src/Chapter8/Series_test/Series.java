fb219c9aced578ba85105ec796baa465e8245d30
MELANCHOLY123456 <moulengdangubulei@gmail.com>
2024-09-29 17:46:49 +0800

    Series_第三次修改

--- src/Chapter8/Series_test/Series.java
+++ src/Chapter8/Series_test/Series.java
@@ -4,6 +4,15 @@ public interface Series {
     int getNext();
 
     default int[] getNextArray(int n) {
+        return getArray(n);
+    }
+
+    default int[] skipAndGetNextArray(int skip, int n) {
+        getArray(skip);
+        return getArray(n);
+    }
+
+    private int[] getArray(int n) {
         int[] vals = new int[n];
         for (int i = 0; i < n; i++) {
             vals[i] = getNext();
